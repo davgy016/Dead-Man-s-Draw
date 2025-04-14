e2637a7de0015c01b57b7fe0d82305cd026f50b4
davgy016 <133870761+davgy016@users.noreply.github.com>
2025-04-14 12:08:44 +0930

    add code in main()

--- DeadManDraw/DeadManDraw.cpp
+++ DeadManDraw/DeadManDraw.cpp
@@ -1,10 +1,26 @@
-
-
+//#include "CardContainer.h"
+//#include "CannonCard.h"
+//#include <vector>
+//#include "PlayArea.h"
+//#include <algorithm>
+//#include <memory>
+#include "Game.h"
 #include <iostream>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Game& game = Game::getInstance();
+    game.initialiseGame();
+    game.startGame();
+
+   /* PlayArea p;
+    Card* c = new CannonCard(3);
+    cout<<c->getType();
+    p.addCard(c);*/   
+    
+    return 0;
 }
 
 
