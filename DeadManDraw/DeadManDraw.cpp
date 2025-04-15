9801b8da6ec7694f7f409731a24200ec23236d98
davgy016 <133870761+davgy016@users.noreply.github.com>
2025-04-15 13:07:56 +0930

    Add game title in main(), in DeadManDraw.cpp

--- DeadManDraw/DeadManDraw.cpp
+++ DeadManDraw/DeadManDraw.cpp
@@ -7,11 +7,30 @@
 #include "Game.h"
 #include <iostream>
 #include "KeyCard.h"
+#ifndef GAME_TITLE_H
+#define GAME_TITLE_H
+#define GAME_TITLE \
+"______                  _   ___  ___              _     \n" \
+"|  _  \\                | |  |  \\/  |             ( )    \n" \
+"| | | | ___   __ _   __| |  | .  . |  __ _  _ __ |/ ___ \n" \
+"| | | |/ _ \\ / _` | / _` |  | |\\/| | / _` || '_ \\  / __|\n" \
+"| |/ /|  __/| (_| || (_| |  | |  | || (_| || | | | \\__ \\ \n" \
+"______ \\___| \\__,_| \\__,_|  \\_|  |_/ \\__,_||_| |_| |___/\n" \
+"|  _  \\                         _      _\n" \
+"| | | | _ __  __ _ __      __ _| |_  _| |_\n" \
+"| | | || '__|/ _` |\\ \\ /\\ / /|_   _||_   _|\n" \
+"| |/ / | |  | (_| | \\ V  V /   |_|    |_|\n" \
+"|___/  |_|   \\__,_|  \\_/\\_/\n"
+
+
+
+#endif //GAME_TITLE_H
 
 using namespace std;
 
 int main()
 {
+    std::cout << GAME_TITLE << std::endl;
     Game& game = Game::getInstance();
     game.initialiseGame();
     game.startGame();
